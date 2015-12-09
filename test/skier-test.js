368762b2517b63559dc95789ae97dfdd6da64da1
roseak <rose.kohn@gmail.com>
2015-12-09 11:57:29 -0700

    Add before action for canvas for tests.

--- test/skier-test.js
+++ test/skier-test.js
@@ -4,35 +4,41 @@ const assert = chai.assert;
 const Skier = require('../app/lib/skier');
 
 describe('Skier', function () {
+  beforeEach(function () {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = 600;
+    this.canvas.height = 500;
+    this.context = this.canvas.getContext('2d');
+  });
 
   it('should be an object', function() {
-    let skier = new Skier({ maxWidth: 600});
+    let skier = new Skier({ canvas: this.canvas, context: this.context});
     assert.isObject(skier);
   });
 
   it('should have an X-coordinate', function() {
-    let skier = new Skier({ maxWidth: 600});
+    let skier = new Skier({ canvas: this.canvas, context: this.context});
     assert.strictEqual(skier.x, 300);
   });
 
   it('should have a Y-coordinate', function() {
-    let skier = new Skier({ maxWidth: 600});
+    let skier = new Skier({ canvas: this.canvas, context: this.context});
     assert.strictEqual(skier.y, 150);
   });
 
   it('should have a width', function() {
-    let skier = new Skier({ maxWidth: 600});
+    let skier = new Skier({ canvas: this.canvas, context: this.context});
     assert.strictEqual(skier.width, 10);
   });
 
   it('should have a height', function() {
-    let skier = new Skier({ maxWidth: 600});
+    let skier = new Skier({ canvas: this.canvas, context: this.context});
     assert.strictEqual(skier.height, 10);
   });
 
   describe('moveLeft', function() {
     it('should move the skier left', function() {
-      let skier = new Skier({ maxWidth: 600});
+      let skier = new Skier({ canvas: this.canvas, context: this.context});
       let skier2 = skier.moveLeft();
       assert.isTrue(skier2.x < skier.maxWidth/2);
     });
@@ -40,7 +46,7 @@ describe('Skier', function () {
 
   describe('moveRight', function() {
     it('should move the skier right', function() {
-      let skier = new Skier({ maxWidth: 600});
+      let skier = new Skier({ canvas: this.canvas, context: this.context});
       let skier2 = skier.moveRight();
       assert.isTrue(skier2.x > skier.maxWidth/2);
     });

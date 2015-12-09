803d1f6a5c1f33a2f175851e98a747aebbeea400
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-09 09:04:11 -0700

    Work on implementing webpack, things are still broken, committing to allow Rose to pull it down.

--- app/lib/index.js
+++ app/lib/index.js
@@ -1,3 +1,3 @@
-const sayHello = () => console.log('Hello');
+var Skifree = require('./skifree');
 
-sayHello();
+new Skifree();
--- app/lib/tree.js
+++ app/lib/tree.js
@@ -1,14 +1,15 @@
 function Tree(options) {
-  this.x = options.maxWidth/2;
+  this.x = options.canvas.width/2;
   this.y = 600;
   this.width = 10;
   this.height = 10;
-  this.maxWidth = options.maxWidth;
+  this.maxWidth = options.canvas.width;
+  this.context = options.context;
 }
 
 Tree.prototype.draw = function() {
-  context.fillStyle="green";
-  context.fillRect(this.x, this.y--, this.width, this.height);
+  this.context.fillStyle="green";
+  this.context.fillRect(this.x, this.y--, this.width, this.height);
   return this;
 };
 

312c028ca9d8df0fa65bc2fea76a591d8716ebf2
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-09 14:48:46 -0700

    Add basic collision detection

--- app/lib/skifree.js
+++ app/lib/skifree.js
@@ -12,6 +12,7 @@ function Skifree () {
   var skier = new Skier({ canvas: canvas, context: context });
   var tree = new Tree({ canvas: canvas, context: context });
   var rock = new Rock({ canvas: canvas, context: context });
+  var bodies = [skier, tree, rock];
 
   function keyPressed(event) {
     if (event.keyCode === 37) {
@@ -21,11 +22,29 @@ function Skifree () {
     }
   }
 
+  var isColliding = function(b1, b2) {
+    if (b1.x === b2.x && b1.y === b2.y) {
+      console.log("collision");
+    }
+  };
+
+  var reportCollisions = function(bodies) {
+    var bodyPairs = [];
+    for (var i = 0; i < bodies.length; i++) {
+      for (var j = i + 1; j < bodies.length; j++) {
+        if (isColliding(bodies[i], bodies[j])) {
+          bodyPairs.push([bodies[i], bodies[j]]);
+        }
+      }
+    }
+  };
+
   requestAnimationFrame(function gameLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     skier.draw();
     tree.draw();
     rock.draw();
+    reportCollisions(bodies);
     requestAnimationFrame(gameLoop);
   });
 }

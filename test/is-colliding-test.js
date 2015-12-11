50c80d244f3b5787a15d34c328e0b2b65a1c40cd
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-11 10:54:49 -0700

    Add more collision tests

--- test/is-colliding-test.js
+++ test/is-colliding-test.js
@@ -11,16 +11,46 @@ describe('isColliding', function() {
     this.canvas.width = 600;
     this.canvas.height = 500;
     this.context = this.canvas.getContext('2d');
+
   });
 
   it('should report if there is a collision', function() {
-    let skier = new Skier({ canvas: this.canvas, context: this.context });
-    let obstacle = new Tree({ canvas: this.canvas, context: this.context });
+    var skier = new Skier({ canvas: this.canvas, context: this.context });
+    var obstacle = new Tree({ canvas: this.canvas, context: this.context });
     skier.x = 50; skier.y = 50;
     obstacle.x = 50; obstacle.y = 50;
 
     assert.strictEqual(skier.crashed, false);
 
+    isColliding(skier, obstacle);
+
+    assert.strictEqual(skier.crashed, true);
+  });
+  it('should report if there is no collision', function() {
+    var skier = new Skier({ canvas: this.canvas, context: this.context });
+    var obstacle = new Tree({ canvas: this.canvas, context: this.context });
+    skier.x = 50; skier.y = 50;
+    obstacle.x = 50; obstacle.y = 30;
+    isColliding(skier, obstacle);
+
+    assert.strictEqual(skier.crashed, false);
+  });
+
+  it('should report based on the skiers width', function() {
+    var skier = new Skier({ canvas: this.canvas, context: this.context });
+    var obstacle = new Tree({ canvas: this.canvas, context: this.context });
+    skier.x = 50; skier.y = 50;
+    obstacle.x = 50; obstacle.y = skier.y - skier.width - 5;
+    isColliding(skier, obstacle);
+
+    assert.strictEqual(skier.crashed, false);
+
+    obstacle.y = skier.y - skier.width;
+    isColliding(skier, obstacle);
+
+    assert.strictEqual(skier.crashed, false);
+
+    obstacle.y = skier.y - skier.width + 1;
     isColliding(skier, obstacle);
 
     assert.strictEqual(skier.crashed, true);

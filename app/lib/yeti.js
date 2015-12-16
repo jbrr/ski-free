8dea95f77b51c2fad763adb2a3d706f889c267c5
roseak <rose.kohn@gmail.com>
2015-12-15 17:37:46 -0700

    Add movement to yeti so it can run left and right.

--- app/lib/yeti.js
+++ app/lib/yeti.js
@@ -5,20 +5,66 @@ function Yeti(options) {
   this.height = 32;
   this.aggressive = false;
   this.context = options.context;
+  this.position = 0;
 }
 
 Yeti.prototype.attack = function(skier, skierImg) {
-  this.context.drawImage(
-    skierImg,
-    0,
-    113,
-    32,
-    41,
-    this.x,
-    this.y,
-    this.height,
-    this.width
-  );
+  this.position += 1;
+  if (this.position > 16) {
+    return this.position = 0;
+  }
+
+  var direction = skier.x - this.x;
+
+  if (this.position < 8 && direction > 0) {
+    this.context.drawImage(
+      skierImg,
+      64,
+      113,
+      25,
+      41,
+      this.x,
+      this.y,
+      this.height,
+      this.width
+    );
+  } else if (this.position > 8 && direction > 0){
+    this.context.drawImage(
+      skierImg,
+      90,
+      113,
+      32,
+      41,
+      this.x,
+      this.y,
+      this.height,
+      this.width
+    );
+  } else if (this.position < 8 && direction < 0){
+    this.context.drawImage(
+      skierImg,
+      64,
+      159,
+      32,
+      41,
+      this.x,
+      this.y,
+      this.height,
+      this.width
+    );
+  } else {
+    this.context.drawImage(
+      skierImg,
+      90,
+      159,
+      32,
+      41,
+      this.x,
+      this.y,
+      this.height,
+      this.width
+    );
+  }
   this.rotation = Math.atan2(skier.y - this.y, skier.x - this.x);
   this.x += Math.cos(this.rotation);
   this.y += Math.sin(this.rotation);

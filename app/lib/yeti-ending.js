6526ffd60e3737db5c7a4880973dfdb19b6d4b0d
roseak <rose.kohn@gmail.com>
2015-12-15 18:47:49 -0700

    Refactor skier drawing and change yeti occurance back to being a lot more rare.

--- app/lib/yeti-ending.js
+++ app/lib/yeti-ending.js
@@ -1,5 +1,5 @@
-var yetiEnding = function(skier, yeti, skierImg, tickCount) {
-  if (skier.distance > 1 && Math.random() > 0.5) {
+var yetiEnding = function(skier, yeti, skierImg) {
+  if (skier.distance > 10000 && Math.random() > 0.9995) {
     yeti.aggressive = true;
   }
 

e35e7e5f6e40be535162b33ce7a613876a112d2f
roseak <rose.kohn@gmail.com>
2015-12-17 10:48:45 -0700

    Add jumps to the actual canvas.

--- app/lib/obstacle-generator.js
+++ app/lib/obstacle-generator.js
@@ -1,5 +1,6 @@
 var Tree = require('./tree');
 var Rock = require('./rock');
+var Jump = require('./jump');
 var drawObstacles = require('./draw-obstacles');
 
 var obstacleGenerator = function(obstacles, skier, canvas, ctx, obstaclesImg, increasedSpeed) {
@@ -9,6 +10,10 @@ var obstacleGenerator = function(obstacles, skier, canvas, ctx, obstaclesImg, in
   if (Math.random() > (0.96 - increasedSpeed/100) && !skier.crashed) {
     obstacles.push(new Tree({ canvas: canvas, context: ctx }));
   }
+  if (Math.random() > (0.99 - increasedSpeed/100) && !skier.crashed) {
+    obstacles.push(new Jump({ canvas: canvas, context: ctx }));
+  }
+
   drawObstacles(obstacles, skier, obstaclesImg, increasedSpeed);
 };
 

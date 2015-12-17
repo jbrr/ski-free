ed1287cf9b1eceb2e20c2cdb1376a3b4800754ec
roseak <rose.kohn@gmail.com>
2015-12-17 11:39:52 -0700

    Add ability for skier to not collide with things when he's jumping vs when he's back down.

--- app/lib/collision/is-colliding.js
+++ app/lib/collision/is-colliding.js
@@ -1,9 +1,19 @@
 var arrayContainsObject = require('./array-contains-object');
 var collisionObstacles = [];
+var Jump = require('./../jump');
 var hypotenuse = require('./hypotenuse');
 
 var isColliding = function(skier, obstacle) {
-  if (hypotenuse(skier, obstacle) < skier.width) {
+  if (hypotenuse(skier, obstacle) < skier.width && obstacle instanceof Jump) {
+    skier.jumping = true;
+    if (cancelableStateTimeout) {
+      clearTimeout(cancelableStateTimeout);
+    }
+    var cancelableStateTimeout = setTimeout(function() {
+      console.log('made it');
+      skier.jumping = false;
+    }, 3000);
+  } else if (hypotenuse(skier, obstacle) < skier.width && skier.jumping === false) {
     skier.crashed = true;
     if (arrayContainsObject(obstacle, collisionObstacles) === false) {
       collisionObstacles.push(obstacle);
